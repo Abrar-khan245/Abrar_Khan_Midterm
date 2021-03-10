faed5deb34fa92a8cc5496a46714f827356b7250
Abrar_Khan <akhan245@uic.edu>
2021-03-10 17:51:17 -0600

    Initial commit

---
+++ src/Midterm.scala
@@ -0,0 +1,13 @@
+object Midterm {
+  def main(args: Array[String]): Unit = {
+    val m = Map("q"->"1","u"->"2","i"->"3","t"->"4")
+    scala.collection.immutable.Map[java.lang.String,Int] = Map(q -> 1, u -> 2,i -> 3,t -> 4)
+      Scope{
+
+
+      }
+
+  }
+
+
+}
